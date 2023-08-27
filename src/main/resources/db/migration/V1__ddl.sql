051e4740407fdd3ec6a8157c6f864874af19b573
VIJAYWHAT <vijayrv042gmail.com>
2023-08-27 09:04:59 +0530

    Check CI

--- src/main/resources/db/migration/V1__ddl.sql
+++ src/main/resources/db/migration/V1__ddl.sql
@@ -1,4 +1,32 @@
-DROP TABLE IF EXISTS org;
+
+
+CREATE TABLE handle (
+    user_handle VARCHAR(200) PRIMARY KEY,
+    type VARCHAR(55),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT type_id_constraint UNIQUE (type, user_handle)
+);
+
+CREATE TABLE learner (
+    user_handle VARCHAR(200) PRIMARY KEY,
+    email VARCHAR(200) NOT NULL,
+    image_url VARCHAR(200) NOT NULL,
+    provider VARCHAR(50) DEFAULT 'local' NOT NULL,
+    password VARCHAR(200),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    modified_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT learner_email_constraint UNIQUE (email),
+    FOREIGN KEY (user_handle) REFERENCES handle (user_handle)
+);
+
+
+CREATE TABLE learner_profile (
+    user_handle VARCHAR(200) PRIMARY KEY,
+    first_name VARCHAR(200) NOT NULL,
+    last_name VARCHAR(200) NOT NULL,
+    dob DATE NOT NULL,
+    FOREIGN KEY (user_handle) REFERENCES handle (user_handle)
+);
 
 CREATE TABLE org (
     id VARCHAR(55) PRIMARY KEY,
@@ -8,4 +36,24 @@ CREATE TABLE org (
     modified_at TIMESTAMP,
     modified_by VARCHAR(200),
     CONSTRAINT org_title_constraint UNIQUE (title)
+);
+
+
+
+CREATE TABLE communities (
+    id VARCHAR(55) PRIMARY KEY,
+    title TEXT,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    created_by VARCHAR(55) NOT NULL,
+    modified_at TIMESTAMP,
+    modified_by VARCHAR(200),
+    CONSTRAINT communities_title_constraint UNIQUE (title)
+);
+
+CREATE TABLE communities_localized (
+    community_id VARCHAR(55),
+    locale VARCHAR(8) NOT NULL,
+    title TEXT,
+    FOREIGN KEY (community_id) REFERENCES communities (id),
+    PRIMARY KEY(community_id, locale)
 );
