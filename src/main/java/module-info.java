0b3416d8ed48154d2a48aec8a0dd8745b2fb17ad
sathishk <sathishkumar.thiyagarajan@gmail.com>
2023-11-01 23:37:03 +0530

    Optimize Imports

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,8 +2,6 @@ module gurukulams.core {
     requires java.base;
     requires java.sql;
     requires java.naming;
-    requires org.json;
-    requires org.postgresql.jdbc;
     requires jakarta.validation;
     requires org.hibernate.validator;
     requires com.h2database;
