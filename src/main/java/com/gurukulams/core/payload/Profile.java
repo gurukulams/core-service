105cba76e0a11d259f21000f1b3c7d3fa3136e1e
sathishk <sathishkumar.thiyagarajan@gmail.com>
2023-12-17 19:09:04 +0530

    Add Follo Service

--- src/main/java/com/gurukulams/core/payload/Profile.java
+++ src/main/java/com/gurukulams/core/payload/Profile.java
@@ -2,8 +2,10 @@ package com.gurukulams.core.payload;
 
 /**
  * Holds Profile of a User Handle.
+ * @param userHandle
  * @param displayName
  * @param profilePicture
  */
-public record Profile(String displayName, String profilePicture) {
+public record Profile(String userHandle, String displayName,
+                      String profilePicture) {
 }
